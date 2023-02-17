fc8d209e0cd68f78d3f9d8637422375aaf20ec39
willbarre <willbarre9@gmail.com>
2023-02-17 12:50:49 +0100

    carto finished

--- lib/Pages/carto.dart
+++ lib/Pages/carto.dart
@@ -26,7 +26,15 @@ class _Carto extends State<Carto> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     void handlePressed(LatLng latLng) {
-      print('hello');
+      var markerIndex =
+          _markers.indexWhere((element) => element.point == latLng);
+
+      if (markerIndex != -1) {
+        print(markerIndex);
+        setState(() {
+          _markers.removeAt(markerIndex);
+        });
+      }
     }
 
     void handleTap(LatLng latLng) {
@@ -36,11 +44,11 @@ class _Carto extends State<Carto> {
               width: 40.0,
               height: 40.0,
               point: latLng,
-              builder: (ctx) => ElevatedButton(
+              builder: (ctx) => IconButton(
                     onPressed: () {
                       handlePressed(latLng);
                     },
-                    child: Icon(Icons.sailing, size: 40, color: Colors.green),
+                    icon: Icon(Icons.sailing, size: 40, color: Colors.green),
                   )),
         );
       });
@@ -50,9 +58,32 @@ class _Carto extends State<Carto> {
       children: [
         Center(
           child: Container(
+            // red circle
+            decoration: BoxDecoration(
+              color: Colors.yellow[700],
+              shape: BoxShape.circle,
+            ),
+            padding: const EdgeInsets.all(16),
+            width: 160,
+            height: 160,
+
+            child: Center(
+              child: Text(
+                'Lorem ipsum',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.blue[900]),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
             margin: EdgeInsets.only(top: 10),
             height: screenHeight * 0.5,
-            width: screenWidth * 0.95,
+            width: screenWidth * 0.75,
             child: FlutterMap(
               options: MapOptions(
                   center: LatLng(44.657978, -1.145238),
@@ -66,8 +97,112 @@ class _Carto extends State<Carto> {
                   urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                   userAgentPackageName: 'com.example.app',
                 ),
+                PolylineLayer(
+                  polylineCulling: false,
+                  polylines: [
+                    Polyline(
+                      points: [
+                        LatLng(44.657978, -1.145238),
+                        LatLng(44.759978, -1.145438),
+                        LatLng(44.658968, -1.345738),
+                      ],
+                      color: Colors.orangeAccent,
+                      strokeWidth: 5,
+                      isDotted: true,
+                    ),
+                  ],
+                ),
+                // This Layer need to be the last to be able to remove markers
                 MarkerLayer(
                   markers: _markers,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 10),
+            height: screenHeight * 0.5,
+            width: screenWidth * 0.75,
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(45.657978, -1.145238),
+                zoom: 10,
+                maxZoom: 18,
+                keepAlive: true,
+                interactiveFlags: ~InteractiveFlag.rotate,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+              ],
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 10),
+            height: screenHeight * 0.5,
+            width: screenWidth * 0.75,
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(45.657978, -0.145238),
+                zoom: 10,
+                maxZoom: 18,
+                keepAlive: true,
+                interactiveFlags: ~InteractiveFlag.rotate,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+              ],
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 10),
+            height: screenHeight * 0.5,
+            width: screenWidth * 0.75,
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(44.657978, -0.145238),
+                zoom: 10,
+                maxZoom: 18,
+                keepAlive: true,
+                interactiveFlags: ~InteractiveFlag.rotate,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+              ],
+            ),
+          ),
+        ),
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 10),
+            height: screenHeight * 0.5,
+            width: screenWidth * 0.75,
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(42.657978, -0.145238),
+                zoom: 10,
+                maxZoom: 18,
+                keepAlive: true,
+                interactiveFlags: ~InteractiveFlag.rotate,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
                 ),
               ],
             ),
