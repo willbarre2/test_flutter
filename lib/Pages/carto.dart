dc8984f35a1a3fdf2b2c8f69276a039011d2c2d0
willbarre <willbarre9@gmail.com>
2023-02-20 12:45:24 +0100

    GPS position diplayed

--- lib/Pages/carto.dart
+++ lib/Pages/carto.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 class Carto extends StatefulWidget {
   const Carto({Key? key}) : super(key: key);
@@ -10,6 +11,8 @@ class Carto extends StatefulWidget {
 }
 
 class _Carto extends State<Carto> {
+  Position? _currentPosition;
+
   List<Marker> _markers = [
     Marker(
       point: LatLng(44.657978, -1.145238),
@@ -21,6 +24,50 @@ class _Carto extends State<Carto> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+
+  void _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Geolocator.getPositionStream().listen((Position position) {
+      setState(() {
+        _currentPosition = position;
+      });
+    });
+  }
+
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
@@ -67,16 +114,29 @@ class _Carto extends State<Carto> {
             width: 160,
             height: 160,
 
-            child: Center(
-              child: Text(
-                'Lorem ipsum',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15,
-                    color: Colors.blue[900]),
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
+            child: _currentPosition != null
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'LAT: ${_currentPosition?.latitude}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.blue[900]),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(
+                        'LNG: ${_currentPosition?.longitude}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                            color: Colors.blue[900]),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  )
+                : CircularProgressIndicator(),
           ),
         ),
         Center(
